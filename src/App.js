64a718272ae5cb4b9566bd5ec3d5738677ec611d
shogo <neko2000shogo@gmail.com>
2023-05-25 20:55:24 +0900

    WebApp

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+	return (
+		<div className="App">
+		<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>	
+		Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+			className="App-link"
+			href="https://reactjs.org"
+			target="_blank"
+			rel="noopener noreferrer"
         >
-          Learn React
+			Learn React
         </a>
-      </header>
+		</header>
     </div>
-  );
+	);
 }
 
 export default App;
