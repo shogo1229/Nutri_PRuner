d7300c10c81ee6790f04775611d9fcb6399c77e5
shogo1229 <neko2000shogo@gmail.com>
2023-05-28 19:59:39 +0900

    5th

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
-import Select from 'react-select';
 import WebGL from './WebGL';
 import Comment from './Comment';
 import Header from './Header';
-import options from './FoodList';
+
 import { FaUtensils } from 'react-icons/fa';
 import './index.css';
 import Gallery from './Gallery';
@@ -14,11 +13,6 @@ const LeftPanel = ({ setCommentData }) => {
 	const [lunchMenuModalIsOpen, setLunchMenuModalIsOpen] = useState(false);
 	const [dinnerMenuModalIsOpen, setDinnerMenuModalIsOpen] = useState(false);
 	const [snackMenuModalIsOpen, setSnackMenuModalIsOpen] = useState(false);
-	const [morningMenu, setMorningMenu] = useState(null);
-	const [lunchMenu, setLunchMenu] = useState(null);
-	const [dinnerMenu, setDinnerMenu] = useState(null);
-	const [snackMenu, setSnackMenu] = useState([]);
-
 
 	const openMorningMenuModal = () => {
 		setMorningMenuModalIsOpen(true);
@@ -52,33 +46,21 @@ const LeftPanel = ({ setCommentData }) => {
 		setSnackMenuModalIsOpen(false);
 	};
 
-	const handleMorningMenuChange = (selectedOptions) => {
-		setMorningMenu(selectedOptions);
-	};
-
-	const handleLunchMenuChange = (selectedOptions) => {
-		setLunchMenu(selectedOptions);
-	};
-
-	const handleDinnerMenuChange = (selectedOptions) => {
-		setDinnerMenu(selectedOptions);
-	};
-
-	const handleSnackMenuChange = (selectedOptions) => {
-		setSnackMenu(selectedOptions);
-	};
 
 	const [selectedDishes, setSelectedDishes] = useState([]);
 
+	const handleSnackMenuChange = (selectedDishes) => {
+		setSelectedDishes(selectedDishes);
+		console.log(selectedDishes)
+	};
+
 	const handleSubmit = () => {
 		const commentData = {
-			morningMenu,
-			lunchMenu,
-			dinnerMenu,
-			snackMenu,
 		};
 		setCommentData(commentData);
 	};
+
+
 
 	return (
 		<div style={{ display: 'flex', height: '100%', flexDirection: 'column' }}>
@@ -88,11 +70,7 @@ const LeftPanel = ({ setCommentData }) => {
 					<h3 style={{ color: 'white', textAlign: 'center', fontSize: '3em', margin: '0' }}>Select Your Morning Menu</h3>
 				</button>
 			</div>
-			{morningMenu && (
-				<div>
-					<h4>Selected Morning Menu: {morningMenu.map((option) => option.label).join(', ')}</h4>
-				</div>
-			)}
+
 
 			<div style={{ flex: '1 1 20%', border: '2px solid black', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 				<button onClick={openLunchMenuModal} className="rounded-full bg-green-500 flex items-center justify-center p-2 flex-row">
@@ -100,11 +78,6 @@ const LeftPanel = ({ setCommentData }) => {
 					<h3 style={{ color: 'white', textAlign: 'center', fontSize: '3em', margin: '0' }}>Select Your Lunch Menu</h3>
 				</button>
 			</div>
-			{lunchMenu && (
-				<div>
-					<h4>Selected Lunch Menu: {lunchMenu.map((option) => option.label).join(', ')}</h4>
-				</div>
-			)}
 
 			<div style={{ flex: '1 1 20%', border: '2px solid black', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 				<button onClick={openDinnerMenuModal} className="rounded-full bg-green-500 flex items-center justify-center p-2 flex-row">
@@ -112,12 +85,6 @@ const LeftPanel = ({ setCommentData }) => {
 					<h3 style={{ color: 'white', textAlign: 'center', fontSize: '3em', margin: '0' }}>Select Your Snack Menu</h3>
 				</button>
 			</div>
-			{dinnerMenu && (
-				<div>
-					<h4>Selected Dinner Menu: {dinnerMenu.map((option) => option.label).join(', ')}</h4>
-				</div>
-			)}
-
 
 			<div style={{ flex: '1 1 20%', border: '2px solid black', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 				<button onClick={openSnackMenuModal} className="rounded-full bg-green-500 flex items-center justify-center p-2 flex-row">
@@ -125,40 +92,45 @@ const LeftPanel = ({ setCommentData }) => {
 					<h3 style={{ color: 'white', textAlign: 'center', fontSize: '3em', margin: '0' }}>Select Your Snack Menu</h3>
 				</button>
 			</div>
-		{/* snackMenuが選択されている場合に表示 */}
-		{snackMenu && (
-		<div>
-			<h4>Selected Snack Menu:</h4>
-			<ul>
-			{snackMenu.map((option) => (
-			<li key={option.value}>{option.label}</li>
-			))}
-			</ul>
-		</div>
-)}
 
-
-			<Modal isOpen={morningMenuModalIsOpen} onRequestClose={closeMorningMenuModal}>
-				<h3>Select Your Morning Menu</h3>
-				<Select options={options} isMulti onChange={handleMorningMenuChange} />
+			<Modal isOpen={morningMenuModalIsOpen} onRequestClose={closeMorningMenuModal}className="modal fixed right-0 top-0 bottom-0 w-1/2 bg-white">
+			<div className="modal-content h-full overflow-y-auto">
+					<h3 className="text-center">Select Your Morning Menu</h3>
+						<div className="container mx-auto p-4">
+							<h1 className="text-2xl font-bold mb-4">Dish Gallery</h1>
+							<Gallery selectedDishes={selectedDishes} setSelectedDishes={handleSnackMenuChange} />
+						</div>
+					</div>
 			</Modal>
 
-			<Modal isOpen={lunchMenuModalIsOpen} onRequestClose={closeLunchMenuModal}>
-				<h3>Select Your Lunch Menu</h3>
-				<Select options={options} isMulti onChange={handleLunchMenuChange} />
+			<Modal isOpen={lunchMenuModalIsOpen} onRequestClose={closeLunchMenuModal}className="modal fixed right-0 top-0 bottom-0 w-1/2 bg-white">
+			<div className="modal-content h-full overflow-y-auto">
+					<h3 className="text-center">Select Your Lunch Menu</h3>
+						<div className="container mx-auto p-4">
+							<h1 className="text-2xl font-bold mb-4">Dish Gallery</h1>
+							<Gallery selectedDishes={selectedDishes} setSelectedDishes={setSelectedDishes} onChange={handleSnackMenuChange} />
+						</div>
+					</div>
 			</Modal>
 
-			<Modal isOpen={dinnerMenuModalIsOpen} onRequestClose={closeDinnerMenuModal}>
-				<h3>Select Your Dinner Menu</h3>
-				<Select options={options} isMulti onChange={handleDinnerMenuChange} />
+			<Modal isOpen={dinnerMenuModalIsOpen} onRequestClose={closeDinnerMenuModal}className="modal fixed right-0 top-0 bottom-0 w-1/2 bg-white">
+			<div className="modal-content h-full overflow-y-auto">
+					<h3 className="text-center">Select Your Dinner Menu</h3>
+						<div className="container mx-auto p-4">
+							<h1 className="text-2xl font-bold mb-4">Dish Gallery</h1>
+							<Gallery selectedDishes={selectedDishes} setSelectedDishes={setSelectedDishes} onChange={handleSnackMenuChange} />
+						</div>
+					</div>
 			</Modal>
 
-			<Modal isOpen={snackMenuModalIsOpen} onRequestClose={closeSnackMenuModal}>
-				<h3>Select Your Snack Menu</h3>
-				<div className="container mx-auto p-4">
-					<h1 className="text-2xl font-bold mb-4">Dish Gallery</h1>
-					<Gallery Gallery selectedDishes={selectedDishes} setSelectedDishes={setSelectedDishes} onChange={handleSnackMenuChange} />
-				</div>
+			<Modal isOpen={snackMenuModalIsOpen} onRequestClose={closeSnackMenuModal} className="modal fixed right-0 top-0 bottom-0 w-1/2 bg-white">
+				<div className="modal-content h-full overflow-y-auto">
+					<h3 className="text-center">Select Your Snack Menu</h3>
+						<div className="container mx-auto p-4">
+							<h1 className="text-2xl font-bold mb-4">Dish Gallery</h1>
+							<Gallery selectedDishes={selectedDishes} setSelectedDishes={setSelectedDishes} onChange={handleSnackMenuChange} />
+						</div>
+					</div>
 			</Modal>
 
 
