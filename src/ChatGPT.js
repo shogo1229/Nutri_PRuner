e1b48b8a74fe763bcc22b1a5a320e0a3283ad73b
ikenaga530 <takumi.ne.jp530@gmail.com>
2023-06-02 18:55:30 +0900

    add unity and chatgpt

--- src/ChatGPT.js
+++ src/ChatGPT.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { chat } from './Chat';  // chat.js のインポート
 
-const ChatGPT = ({index}) => {  
+const ChatGPT = ({index}) => {
+	// メッセージの状態管理用
+	const [ message, setMessage ] = useState( '' );
+	// 回答の状態管理用
+	const [ answer, setAnswer ] = useState( '' );
+
+	// メッセージの格納
+	const handleMessageChange = ( event )  => {
+		setMessage( event.target.value );
+	}
+
+	// 「質問」ボタンを押したときの処理
+	const handleSubmit = async ( event ) => {
+		event.preventDefault();
+
+		// chat.js にメッセージを渡して API から回答を取得
+		const responseText = await chat( message );
+
+		// 回答の格納
+		setAnswer( responseText );
+	}
+
+	// チャットフォームの表示
 	return (
-		<>
-		<p>Fuck Web WebGL</p>
-		<h1>{index}</h1>
-		</>
+		<div className="overflow-y-auto">
+			<h1>{index}</h1>
+			<form onSubmit={ handleSubmit }>
+				<label>
+					<textarea
+						rows='5'
+						cols='50'
+						value={ message }
+						onChange={ handleMessageChange }
+					/>
+				</label>
+				<div>
+					<button type="submit">質問する</button>
+				</div>
+			</form>
+				{ answer && (
+					<div>
+						<h2>回答:</h2>
+						<p>{ answer }</p>
+					</div>
+				) }
+		</div>
+	);
+}
 
-		);
-	};
+// const ChatGPT = ({index}) => {  
+// 	return (
+// 		<>
+// 		<p>Fuck Web WebGL</p>
+// 		<h1>{index}</h1>
+// 		</>
+
+// 		);
+// 	};
 
 export default ChatGPT;
--- src/WebGL.js
+++ src/WebGL.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
-const WebGL = ({index}) => {  
+function WebGL({index}) {
+	const { unityProvider, sendMessage, addEventListener, removeEventListener } =
+    useUnityContext({
+		loaderUrl: "unity/build0602_webgl_2.loader.js",
+		dataUrl: "unity/build0602_webgl_2.data",
+		frameworkUrl: "unity/build0602_webgl_2.framework.js",
+		codeUrl: "unity/build0602_webgl_2.wasm",
+    });
+	function handleClick() {
+		//sendMessage("3sou", "transformKoma", 0.5);
+		sendMessage("3sou", "scenechange",1)
+		// sendMessage("3rd", "transformKoma", 2.0);
+		// sendMessage("1st_left", "transformKoma", 2.0);
+		// sendMessage("2nd", "transformKoma", 2.0);
+		// sendMessage("4th", "transformKoma", 2.0);
+		// sendMessage("1st_right", "transformKoma", 2.0);
+
+	}
+
 	return (
-		<>
-		<p>Fuck Web WebGL</p>
-		<h1>{index}</h1>
-		</>
-
-		);
-	};
+		<Fragment>
+			<Unity unityProvider={unityProvider} style={{width:"100%",height:"70vh"}}/>
+			<button onClick={handleClick}>Spawn Enemies</button>
+		</Fragment>
+	);
+}
 
 export default WebGL;
+// const WebGL = ({index}) => {  
+// 	return (
+// 		<>
+// 		<p>Fuck Web WebGL</p>
+// 		<h1>{index}</h1>
+// 		</>
+
+// 		);
+// 	};
+
+// export default WebGL;
