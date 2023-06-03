ba205e8c514e3ba66061615790f95140bd5f6a4e
shogo1229 <neko2000shogo@gmail.com>
2023-06-03 23:49:38 +0900

    amplify env

--- src/Chat.js
+++ src/Chat.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = "https://api.openai.com/v1/";
 const MODEL = "gpt-3.5-turbo";
-const API_KEY = process.env.CHAT_KEY;
+const API_KEY = process.env.REACT_APP_CHAT;
 
 export const chat = async (message) => {
   console.log("Call2API",message)
