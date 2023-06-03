25076b4b826e087a13d2f8b150b3008e98208d69
ikenaga530 <takumi.ne.jp530@gmail.com>
2023-06-03 12:41:36 +0900

    modify chat

--- src/ChatGPT.js
+++ src/ChatGPT.js
@@ -1,51 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { chat } from './Chat';  // chat.js のインポート
+import { useSelector } from "react-redux";
 
 const ChatGPT = ({index}) => {
 	// メッセージの状態管理用
 	const [ message, setMessage ] = useState( '' );
 	// 回答の状態管理用
 	const [ answer, setAnswer ] = useState( '' );
+	const chatFlag = useSelector((state) => state.setToState);
 
-	// メッセージの格納
-	const handleMessageChange = ( event )  => {
-		setMessage( event.target.value );
-	}
+	useEffect(async () => {
+		if(chatFlag) {
+			const message_body = index[0];
+			setMessage(message_body);
+			const responseText = await chat (message);
+			console.log(responseText)
+			setAnswer(responseText);
+		}
+	},[chatFlag]);
 
-	// 「質問」ボタンを押したときの処理
-	const handleSubmit = async ( event ) => {
-		event.preventDefault();
 
-		// chat.js にメッセージを渡して API から回答を取得
-		const responseText = await chat( message );
+	// // メッセージの格納
+	// const handleMessageChange = ( event )  => {
+	// 	setMessage( event.target.value );
+	// }
 
-		// 回答の格納
-		setAnswer( responseText );
-	}
+	// // 「質問」ボタンを押したときの処理
+	// const handleSubmit = async ( event ) => {
+	// 	event.preventDefault();
+
+	// 	// chat.js にメッセージを渡して API から回答を取得
+	// 	const responseText = await chat( message );
+
+	// 	// 回答の格納
+	// 	setAnswer( responseText );
+	// }
 
 	// チャットフォームの表示
 	return (
 		<div className="overflow-y-auto">
 			<h1>{index}</h1>
-			<form onSubmit={ handleSubmit }>
-				<label>
-					<textarea
-						rows='5'
-						cols='50'
-						value={ message }
-						onChange={ handleMessageChange }
-					/>
-				</label>
+			{ answer && (
 				<div>
-					<button type="submit">質問する</button>
+					<h2>回答:</h2>
+					<p>{ answer }</p>
 				</div>
-			</form>
-				{ answer && (
-					<div>
-						<h2>回答:</h2>
-						<p>{ answer }</p>
-					</div>
-				) }
+			)}
 		</div>
 	);
 }
