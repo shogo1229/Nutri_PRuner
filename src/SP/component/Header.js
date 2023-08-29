8e3c896a4f0091ae346b65af2b2053d0f5fb0435
shogo1229 <neko2000shogo@gmail.com>
2023-08-30 01:17:00 +0900

    新規Headerに変更

--- src/SP/component/Header.js
+++ src/SP/component/Header.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import TutorialModal from "./ModalOpen_tutorial";
 import unity_run from "../img/Unitychan_run.gif";
+import info_icon from "../img/info.png";
 
 const Header = () => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const handleInfoClick = () => {
+    setShowInfo(!showInfo);
+  };
+
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      <img src={unity_run} alt="画像の説明" />
-      <p className="font-bold text-xl bg-[#F3F3F3] text-[#37AB9D]">
-        Nutri PRuner
-      </p>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src={unity_run}
+          alt="UnityChanAnimation"
+          style={{ width: "75px", height: "75px" }}
+        />
+        <p
+          className="font-bold text-3xl bg-[#F3F3F3] text-[#37AB9D]"
+          style={{ marginTop: "10px", marginLeft: "10px" }}
+        >
+          Nutri PRuner
+        </p>
+      </div>
+      <img
+        src={info_icon}
+        alt="Infomaiton Icon"
+        style={{
+          width: "40px",
+          height: "40px",
+          cursor: "pointer",
+          marginTop: "15px",
+        }}
+        onClick={handleInfoClick}
+      />
+      {showInfo && <TutorialModal />}
     </div>
   );
 };
