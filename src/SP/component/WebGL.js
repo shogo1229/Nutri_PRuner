3e1de6117ff47db6b2f81cb3fd2edfb524ab54ed
shogo1229 <neko2000shogo@gmail.com>
2023-08-31 03:09:42 +0900

    WebGL2 SP動作確認

--- src/SP/component/WebGL.js
+++ src/SP/component/WebGL.js
@@ -5,10 +5,10 @@ import { useSelector } from "react-redux";
 function WebGL({ index }) {
   const { unityProvider, sendMessage, addEventListener, removeEventListener } =
     useUnityContext({
-      loaderUrl: "unity/WebGL_SP.loader.js",
-      dataUrl: "unity/WebGL_SP.data",
-      frameworkUrl: "unity/WebGL_SP.framework.js",
-      codeUrl: "unity/WebGL_SP.wasm",
+      loaderUrl: "unity/Hakkason_webGL2_Z10.loader.js",
+      dataUrl: "unity/Hakkason_webGL2_Z10.data",
+      frameworkUrl: "unity/Hakkason_webGL2_Z10.framework.js",
+      codeUrl: "unity/Hakkason_webGL2_Z10.wasm",
     });
 
   const [state, setFlag] = useState(false);
