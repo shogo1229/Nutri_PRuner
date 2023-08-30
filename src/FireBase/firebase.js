80c824fcd2e29fc83e0d2f52784f4c29583c6e3f
shogo1229 <neko2000shogo@gmail.com>
2023-08-31 01:00:48 +0900

    firebase修正

--- src/FireBase/firebase.js
+++ src/FireBase/firebase.js
@@ -1,17 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import config from "../config";
 
+const API_KEY = process.env.REACT_APP_FIREBASE;
+const AUTHDOMAIN = process.env.REACT_APP_AUTHDOMAIN;
+const PROJECT_ID = process.env.REACT_APP_PROJECTID;
+const STORAGE_BUCKET = process.env.REACT_APP_STORAGEBUCKET;
+const MESSAGINGSENDER_ID = process.env.REACT_APP_MESSAGINGSENDERID;
+const APP_ID = process.env.REACT_APP_APPID;
+const MEASUREMENT_ID = process.env.REACT_APP_MEASUREMENTID;
 //就活用アカウントのFireBase
 const firebaseConfig = {
-  apiKey: config.FireBase_apiKey,
-  authDomain: config.FireBase_authDomain,
-  projectId: config.FireBase_projectId,
-  storageBucket: config.FireBase_storageBucket,
-  messagingSenderId: config.FireBase_messagingSenderId,
-  appId: config.FireBase_appId,
-  measurementId: config.FireBase_measurementId,
+  apiKey: API_KEY,
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGINGSENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
