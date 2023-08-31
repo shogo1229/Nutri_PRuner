43edbad5f83a454685d53f50e86aad441f46ee58
shogo1229 <neko2000shogo@gmail.com>
2023-08-31 20:29:07 +0900

    動作確認

--- src/FireBase/firebase.js
+++ src/FireBase/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import config from "../config";
 
 const API_KEY = process.env.REACT_APP_FIREBASE;
 const AUTHDOMAIN = process.env.REACT_APP_AUTHDOMAIN;
@@ -19,6 +20,17 @@ const firebaseConfig = {
   appId: APP_ID,
   measurementId: MEASUREMENT_ID,
 };
+
+//就活用アカウントのFireBase
+// const firebaseConfig = {
+//   apiKey: config.FireBase_apiKey,
+//   authDomain: config.FireBase_authDomain,
+//   projectId: config.FireBase_projectId,
+//   storageBucket: config.FireBase_storageBucket,
+//   messagingSenderId: config.FireBase_messagingSenderId,
+//   appId: config.FireBase_appId,
+//   measurementId: config.FireBase_measurementId,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
