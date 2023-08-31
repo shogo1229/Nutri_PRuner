8c5cb9521b981f7b15cea8cb8d5924895615acbe
shogo1229 <neko2000shogo@gmail.com>
2023-08-31 11:48:53 +0900

    画像配置前コードのみ変更

--- src/SP/component/WebGL.js
+++ src/SP/component/WebGL.js
@@ -1,23 +1,19 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import { useSelector } from "react-redux";
 
-function WebGL({ index }) {
-  const { unityProvider, sendMessage, addEventListener, removeEventListener } =
-    useUnityContext({
-      loaderUrl: "unity/Hakkason_webGL2_build.loader.js",
-      dataUrl: "unity/Hakkason_webGL2_build.data",
-      frameworkUrl: "unity/Hakkason_webGL2_build.framework.js",
-      codeUrl: "unity/Hakkason_webGL2_build.wasm",
-    });
+import UnityNormalGIF from "../img/UnityRun_Normal.gif";
+import UnityfalldownGIF from "../img/UnityRun_falldown.gif";
+import UnityguraguraGIF from "../img/UnityRun_guragura.gif";
 
+function WebGL({ index }) {
   const [state, setFlag] = useState(false);
   const [buttonState, setButtonFlag] = useState(true);
-  const loadFlag = useSelector((state) => state.setToState);
   const unityStyle = {
     width: "100%",
-    height: "70vh",
-    //visibility: state ? "visible" : "hidden",
+    height: "100%",
+    visibility: state ? "visible" : "hidden",
   };
 
   let org_index = index.slice();
@@ -34,67 +30,36 @@ function WebGL({ index }) {
 
   let Unity_scene = 0;
 
-  if (sum === 0) {
-    Unity_scene = 0;
-  } else if (1 < sum && sum < 3) {
-    Unity_scene = 1;
-  } else if (3 < sum && sum < 6) {
-    Unity_scene = 2;
-  } else if (6 < sum && sum < 8) {
-    Unity_scene = 3;
-  } else if (8 < sum && sum < 10) {
-    Unity_scene = 4;
-  } else if (sum > 10) {
-    Unity_scene = 5;
-  }
-
-  console.log("Use Scene", Unity_scene);
-  useEffect(() => {
-    if (loadFlag) {
-      sendMessage("1sou_right", "scenechange", Unity_scene);
-    }
-  }, [loadFlag]);
-
-  const changeSize = () => {
-    const sou1_right = compareValues(org_index[3]);
-    const sou1_left = compareValues(org_index[4]);
-    const sou2 = compareValues(org_index[2]);
-    const sou3 = compareValues(org_index[1]);
-    const sou4 = compareValues(org_index[0]);
-
-    //大きさを変更
-    sendMessage("1sou_right", "transformKoma", sou1_right);
-    sendMessage("1sou_left", "transformKoma", sou1_left);
-    sendMessage("2sou", "transformKoma", sou2);
-    sendMessage("3sou", "transformKoma", sou3);
-    sendMessage("4sou", "transformKoma", sou4);
-    setFlag(() => true);
-  };
-
-  console.log(Unity_scene);
-
-  const compareValues = (value) => {
-    if (value === 0) {
-      console.log("just");
-      return 1;
-    } else if (value < 0) {
-      let normalized_minus_value = Math.abs(value) / (Math.abs(value) + 1);
-      let converted_minus_value = 0.4 + normalized_minus_value * 0.6;
-      console.log("WebGL_Plus", value, converted_minus_value);
-      return converted_minus_value;
-    } else if (value > 0) {
-      let normalized_plus_value = value / (value + 0.6);
-      let converted_plus_value = 0.6 + normalized_plus_value;
-      console.log("WebGL_Minus", value, converted_plus_value);
-      return converted_plus_value;
-    }
-  };
+  // if (sum === 0) {
+  //   Unity_scene = 0;
+  // } else if (1 < sum && sum < 3) {
+  //   Unity_scene = 1;
+  // } else if (3 < sum && sum < 6) {
+  //   Unity_scene = 2;
+  // } else if (6 < sum && sum < 8) {
+  //   Unity_scene = 3;
+  // } else if (8 < sum && sum < 10) {
+  //   Unity_scene = 4;
+  // } else if (sum > 10) {
+  //   Unity_scene = 5;
+  // }
 
   return (
     <>
       <div style={{ position: "relative" }}>
-        <Unity unityProvider={unityProvider} style={unityStyle} />
-        {/* <div
+        <img
+          src={
+            [
+              UnityNormalGIF,
+              UnityfalldownGIF,
+              UnityguraguraGIF,
+              UnityNormalGIF,
+              UnityNormalGIF,
+            ][Unity_scene]
+          }
+          style={unityStyle}
+        />
+        <div
           style={{
             position: "absolute",
             top: "50%",
@@ -113,13 +78,13 @@ function WebGL({ index }) {
               className="relative w-full text-left font-bold text-32 text-white transition-colors duration-200 ease-in-out group-hover:text-[#37AB9D]"
               onClick={() => {
                 setButtonFlag(() => false);
-                changeSize();
+                setFlag(() => true);
               }}
             >
               Result
             </button>
           </a>
-        </div> */}
+        </div>
       </div>
     </>
   );
